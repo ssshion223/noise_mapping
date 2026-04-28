394ff5d015ee95b581ced7ea1db78dccff6bf527
sshion <sion2661791402@gmail.com>
2026-04-28 09:10:29 +0800

    ff

--- src/noise_mapping/src/noise_mapping.cpp
+++ src/noise_mapping/src/noise_mapping.cpp
@@ -1,7 +1,16 @@
 #include "noise_mapping.hpp"
 
 
-grid_map::GridMap map({"energy"});
-map.setFrameId("map");
-map.setGeometry(grid_map::Length(5.0, 5.0), 0.1);
+GridMapNode::GridMapNode(): Node("grid_map_node") {
 
+  pub_ = this->create_publisher<grid_map_msgs::msg::GridMap>("/grid_map", 10);
+  timer_ = this->create_wall_timer(std::chrono::milliseconds(1000), std::bind(&GridMapNode::grid_map_cb, this));
+}
+
+GridMapNode::~GridMapNode(){}
+
+
+void GridMapNode::grid_map_cb(){
+  grid_map_msgs::msg::GridMap msg;
+  msg.
+}
--- src/noise_mapping/src/noise_mapping.hpp
+++ src/noise_mapping/src/noise_mapping.hpp
@@ -3,15 +3,18 @@
 #include <grid_map_core/GridMap.hpp>
 #include <grid_map_core/iterators/GridMapIterator.hpp>
 #include <grid_map_ros/grid_map_ros.hpp>
-
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 class GridMapNode :public rclcpp::Node {
 public:
   GridMapNode();
   ~GridMapNode();
 private:  //private function
-
+  void grid_map_cb();
 private:  //private member
+  grid_map::GridMap map_({"energy"});
   rclcpp::Publisher<grid_map_msgs::msg::GridMap>::SharedPtr pub_;
   rclcpp::TimerBase::SharedPtr timer_;
 };
